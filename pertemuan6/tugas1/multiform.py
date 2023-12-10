c701c9e9c4e24f2ce7194feed079cca0ecad4c6d
Rifki Saifullah <112708909+rifki-saifullah@users.noreply.github.com>
2023-12-10 21:35:38 +0700

    Update multiform.py

--- pertemuan6/tugas1/multiform.py
+++ pertemuan6/tugas1/multiform.py
@@ -1,273 +1,45 @@
 import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
-import math
+from tkinter import ttk 
+from function.include import *
 
-class CalculatorApp:
-    def __init__(self, master):
-        self.master = master
-        self.master.title("Calculator")
-        self.master.geometry("700x300")
+def new_window(_class):
+	new = tk.Toplevel()
+	new.transient()
+	new.grab_set()
+	_class(new)
 
-        self.notebook = ttk.Notebook(self.master)
-        self.notebook.pack(fill=tk.BOTH, expand=True)
+# Window
+window = tk.Tk()
+window.title("Multiform")
+window.geometry("300x200")
 
-        self.create_tab("Balok", self.calculate_balok)
-        self.create_tab("Bola", self.calculate_bola)
-        self.create_tab("Kerucut", self.calculate_kerucut)
-        self.create_tab("Kubus", self.calculate_kubus)
-        self.create_tab("Limas Segiempat", self.calculate_limas_segiempat)
-        self.create_tab("Limas Segitiga", self.calculate_limas_segitiga)
-        self.create_tab("Prisma Segitiga", self.calculate_prisma_segitiga)
-        self.create_tab("Tabung", self.calculate_tabung)
+# Frame
+frame = ttk.Frame(window)
+frame.pack(padx=10, pady=10)
 
-    def create_tab(self, title, callback):
-        tab = ttk.Frame(self.notebook)
-        self.notebook.add(tab, text=title)
+nama = ttk.Label(frame, text="Rifki Saifullah")
+nama.grid(row=0, column=0, sticky=tk.W, padx=10, pady=10)
 
-        callback(tab)
+nim = ttk.Label(frame, text="220511145")
+nim.grid(row=1, column=0, sticky=tk.W, padx=10, pady=10)
 
-    def calculate_balok(self, tab):
-        label_panjang = tk.Label(tab, text="Panjang:")
-        entry_panjang = tk.Entry(tab)
-        label_panjang.grid(row=0, column=0, padx=10, pady=10)
-        entry_panjang.grid(row=0, column=1, padx=10, pady=10)
+kelas = ttk.Label(frame, text="R5")
+kelas.grid(row=2, column=0, sticky=tk.W, padx=10, pady=10)
 
-        label_lebar = tk.Label(tab, text="Lebar:")
-        entry_lebar = tk.Entry(tab)
-        label_lebar.grid(row=1, column=0, padx=10, pady=10)
-        entry_lebar.grid(row=1, column=1, padx=10, pady=10)
+# Bar Menu
+menu = tk.Menu(window)
+window.config(menu=menu)
 
-        label_tinggi = tk.Label(tab, text="Tinggi:")
-        entry_tinggi = tk.Entry(tab)
-        label_tinggi.grid(row=2, column=0, padx=10, pady=10)
-        entry_tinggi.grid(row=2, column=1, padx=10, pady=10)
+# Menu Calculator
+menu_calculator = tk.Menu(menu, tearoff=False)
+menu.add_cascade(label="Kalkulator Bangun Ruang", menu=menu_calculator)
+menu_calculator.add_command(label="Balok", command=lambda: new_window(balok))
+menu_calculator.add_command(label="Bola", command=lambda: new_window(bola))
+menu_calculator.add_command(label="Kerucut", command=lambda: new_window(kerucut))
+menu_calculator.add_command(label="Kubus", command=lambda: new_window(kubus))
+menu_calculator.add_command(label="Limas Segiempat", command=lambda: new_window(limas_segiempat))
+menu_calculator.add_command(label="Limas Segitiga", command=lambda: new_window(limas_segitiga))
+menu_calculator.add_command(label="Prisma Segitiga", command=lambda: new_window(prisma_segitiga))
+menu_calculator.add_command(label="Tabung", command=lambda: new_window(tabung))
 
-        result_label = tk.Label(tab, text="")
-        result_label.grid(row=3, column=0, columnspan=2, pady=10)
-
-        calculate_button = tk.Button(tab, text="Hitung", command=lambda: self.calculate_result_balok(entry_panjang.get(), entry_lebar.get(), entry_tinggi.get(), result_label))
-        calculate_button.grid(row=4, column=0, columnspan=2, pady=10)
-
-    def calculate_result_balok(self, panjang, lebar, tinggi, result_label):
-        try:
-            panjang = float(panjang)
-            lebar = float(lebar)
-            tinggi = float(tinggi)
-
-            luas = 2 * (panjang * lebar + panjang * tinggi + lebar * tinggi)
-            volume = panjang * lebar * tinggi
-
-            result_text = f"Luas: {luas:.2f}, Volume: {volume:.2f}"
-            result_label.config(text=result_text)
-        except ValueError:
-            messagebox.showerror("Error", "Masukkan angka yang valid")
-
-    def calculate_bola(self, tab):
-        label_jari = tk.Label(tab, text="Jari-jari:")
-        entry_jari = tk.Entry(tab)
-        label_jari.grid(row=0, column=0, padx=10, pady=10)
-        entry_jari.grid(row=0, column=1, padx=10, pady=10)
-
-        result_label = tk.Label(tab, text="")
-        result_label.grid(row=1, column=0, columnspan=2, pady=10)
-
-        calculate_button = tk.Button(tab, text="Hitung", command=lambda: self.calculate_result_bola(entry_jari.get(), result_label))
-        calculate_button.grid(row=2, column=0, columnspan=2, pady=10)
-
-    def calculate_result_bola(self, jari, result_label):
-        try:
-            jari = float(jari)
-
-            luas = 4 * 3.14 * (jari ** 2)
-            volume = (4 / 3) * 3.14 * (jari ** 3)
-
-            result_text = f"Luas: {luas:.2f}, Volume: {volume:.2f}"
-            result_label.config(text=result_text)
-        except ValueError:
-            messagebox.showerror("Error", "Masukkan angka yang valid")
-
-    def calculate_kerucut(self, tab):
-        label_jari_kerucut = tk.Label(tab, text="Jari-jari:")
-        entry_jari_kerucut = tk.Entry(tab)
-        label_jari_kerucut.grid(row=0, column=0, padx=10, pady=10)
-        entry_jari_kerucut.grid(row=0, column=1, padx=10, pady=10)
-
-        label_tinggi_kerucut = tk.Label(tab, text="Tinggi:")
-        entry_tinggi_kerucut = tk.Entry(tab)
-        label_tinggi_kerucut.grid(row=1, column=0, padx=10, pady=10)
-        entry_tinggi_kerucut.grid(row=1, column=1, padx=10, pady=10)
-
-        result_label = tk.Label(tab, text="")
-        result_label.grid(row=2, column=0, columnspan=2, pady=10)
-
-        calculate_button = tk.Button(tab, text="Hitung", command=lambda: self.calculate_result_kerucut(entry_jari_kerucut.get(), entry_tinggi_kerucut.get(), result_label))
-        calculate_button.grid(row=3, column=0, columnspan=2, pady=10)
-
-    def calculate_result_kerucut(self, jari, tinggi, result_label):
-        try:
-            jari = float(jari)
-            tinggi = float(tinggi)
-
-            luas = math.pi * jari * (jari + math.sqrt(jari**2 + tinggi**2))
-            volume = (1 / 3) * math.pi * (jari**2) * tinggi
-
-            result_text = f"Luas: {luas:.2f}, Volume: {volume:.2f}"
-            result_label.config(text=result_text)
-        except ValueError:
-            messagebox.showerror("Error", "Masukkan angka yang valid")
-
-    def calculate_kubus(self, tab):
-        label_sisi_kubus = tk.Label(tab, text="Sisi:")
-        entry_sisi_kubus = tk.Entry(tab)
-        label_sisi_kubus.grid(row=0, column=0, padx=10, pady=10)
-        entry_sisi_kubus.grid(row=0, column=1, padx=10, pady=10)
-
-        result_label = tk.Label(tab, text="")
-        result_label.grid(row=1, column=0, columnspan=2, pady=10)
-
-        calculate_button = tk.Button(tab, text="Hitung", command=lambda: self.calculate_result_kubus(entry_sisi_kubus.get(), result_label))
-        calculate_button.grid(row=2, column=0, columnspan=2, pady=10)
-
-    def calculate_result_kubus(self, sisi, result_label):
-        try:
-            sisi = float(sisi)
-
-            luas = 6 * (sisi**2)
-            volume = sisi**3
-
-            result_text = f"Luas: {luas:.2f}, Volume: {volume:.2f}"
-            result_label.config(text=result_text)
-        except ValueError:
-            messagebox.showerror("Error", "Masukkan angka yang valid")
-
-    def calculate_limas_segiempat(self, tab):
-        label_sisi_limas_segiempat = tk.Label(tab, text="Sisi Alas:")
-        entry_sisi_limas_segiempat = tk.Entry(tab)
-        label_sisi_limas_segiempat.grid(row=0, column=0, padx=10, pady=10)
-        entry_sisi_limas_segiempat.grid(row=0, column=1, padx=10, pady=10)
-
-        label_tinggi_limas_segiempat = tk.Label(tab, text="Tinggi:")
-        entry_tinggi_limas_segiempat = tk.Entry(tab)
-        label_tinggi_limas_segiempat.grid(row=1, column=0, padx=10, pady=10)
-        entry_tinggi_limas_segiempat.grid(row=1, column=1, padx=10, pady=10)
-
-        result_label = tk.Label(tab, text="")
-        result_label.grid(row=2, column=0, columnspan=2, pady=10)
-
-        calculate_button = tk.Button(tab, text="Hitung", command=lambda: self.calculate_result_limas_segiempat(entry_sisi_limas_segiempat.get(), entry_tinggi_limas_segiempat.get(), result_label))
-        calculate_button.grid(row=3, column=0, columnspan=2, pady=10)
-
-    def calculate_result_limas_segiempat(self, sisi_alas, tinggi, result_label):
-        try:
-            sisi_alas = float(sisi_alas)
-            tinggi = float(tinggi)
-
-            luas = sisi_alas**2 + 4 * (sisi_alas * tinggi) / 2
-            volume = (1 / 3) * (sisi_alas**2) * tinggi
-
-            result_text = f"Luas: {luas:.2f}, Volume: {volume:.2f}"
-            result_label.config(text=result_text)
-        except ValueError:
-            messagebox.showerror("Error", "Masukkan angka yang valid")
-
-    def calculate_limas_segitiga(self, tab):
-        label_sisi_limas_segitiga = tk.Label(tab, text="Sisi Alas:")
-        entry_sisi_limas_segitiga = tk.Entry(tab)
-        label_sisi_limas_segitiga.grid(row=0, column=0, padx=10, pady=10)
-        entry_sisi_limas_segitiga.grid(row=0, column=1, padx=10, pady=10)
-
-        label_tinggi_limas_segitiga = tk.Label(tab, text="Tinggi:")
-        entry_tinggi_limas_segitiga = tk.Entry(tab)
-        label_tinggi_limas_segitiga.grid(row=1, column=0, padx=10, pady=10)
-        entry_tinggi_limas_segitiga.grid(row=1, column=1, padx=10, pady=10)
-
-        result_label = tk.Label(tab, text="")
-        result_label.grid(row=2, column=0, columnspan=2, pady=10)
-
-        calculate_button = tk.Button(tab, text="Hitung", command=lambda: self.calculate_result_limas_segitiga(entry_sisi_limas_segitiga.get(), entry_tinggi_limas_segitiga.get(), result_label))
-        calculate_button.grid(row=3, column=0, columnspan=2, pady=10)
-
-    def calculate_result_limas_segitiga(self, sisi_alas, tinggi, result_label):
-        try:
-            sisi_alas = float(sisi_alas)
-            tinggi = float(tinggi)
-
-            luas = sisi_alas * (sisi_alas + math.sqrt(3) * tinggi**2)
-            volume = (1 / 3) * (sisi_alas**2) * tinggi
-
-            result_text = f"Luas: {luas:.2f}, Volume: {volume:.2f}"
-            result_label.config(text=result_text)
-        except ValueError:
-            messagebox.showerror("Error", "Masukkan angka yang valid")
-
-    def calculate_prisma_segitiga(self, tab):
-        label_sisi_alas_prisma = tk.Label(tab, text="Sisi Alas:")
-        entry_sisi_alas_prisma = tk.Entry(tab)
-        label_sisi_alas_prisma.grid(row=0, column=0, padx=10, pady=10)
-        entry_sisi_alas_prisma.grid(row=0, column=1, padx=10, pady=10)
-
-        label_tinggi_prisma = tk.Label(tab, text="Tinggi:")
-        entry_tinggi_prisma = tk.Entry(tab)
-        label_tinggi_prisma.grid(row=1, column=0, padx=10, pady=10)
-        entry_tinggi_prisma.grid(row=1, column=1, padx=10, pady=10)
-
-        label_tinggi_segitiga_prisma = tk.Label(tab, text="Tinggi Segitiga Alas:")
-        entry_tinggi_segitiga_prisma = tk.Entry(tab)
-        label_tinggi_segitiga_prisma.grid(row=2, column=0, padx=10, pady=10)
-        entry_tinggi_segitiga_prisma.grid(row=2, column=1, padx=10, pady=10)
-
-        result_label = tk.Label(tab, text="")
-        result_label.grid(row=3, column=0, columnspan=2, pady=10)
-
-        calculate_button = tk.Button(tab, text="Hitung", command=lambda: self.calculate_result_prisma_segitiga(entry_sisi_alas_prisma.get(), entry_tinggi_prisma.get(), entry_tinggi_segitiga_prisma.get(), result_label))
-        calculate_button.grid(row=4, column=0, columnspan=2, pady=10)
-
-    def calculate_result_prisma_segitiga(self, sisi_alas, tinggi_prisma, tinggi_segitiga, result_label):
-        try:
-            sisi_alas = float(sisi_alas)
-            tinggi_prisma = float(tinggi_prisma)
-            tinggi_segitiga = float(tinggi_segitiga)
-
-            luas = sisi_alas * (tinggi_prisma + tinggi_segitiga)
-            volume = (1 / 2) * sisi_alas * tinggi_prisma * tinggi_segitiga
-
-            result_text = f"Luas: {luas:.2f}, Volume: {volume:.2f}"
-            result_label.config(text=result_text)
-        except ValueError:
-            messagebox.showerror("Error", "Masukkan angka yang valid")
-
-    def calculate_tabung(self, tab):
-        label_jari_tabung = tk.Label(tab, text="Jari-jari:")
-        entry_jari_tabung = tk.Entry(tab)
-        label_jari_tabung.grid(row=0, column=0, padx=10, pady=10)
-        entry_jari_tabung.grid(row=0, column=1, padx=10, pady=10)
-
-        label_tinggi_tabung = tk.Label(tab, text="Tinggi:")
-        entry_tinggi_tabung = tk.Entry(tab)
-        label_tinggi_tabung.grid(row=1, column=0, padx=10, pady=10)
-        entry_tinggi_tabung.grid(row=1, column=1, padx=10, pady=10)
-
-        result_label = tk.Label(tab, text="")
-        result_label.grid(row=2, column=0, columnspan=2, pady=10)
-
-        calculate_button = tk.Button(tab, text="Hitung", command=lambda: self.calculate_result_tabung(entry_jari_tabung.get(), entry_tinggi_tabung.get(), result_label))
-        calculate_button.grid(row=3, column=0, columnspan=2, pady=10)
-
-    def calculate_result_tabung(self, jari, tinggi, result_label):
-        try:
-            jari = float(jari)
-            tinggi = float(tinggi)
-
-            luas = 2 * math.pi * jari * (jari + tinggi)
-            volume = math.pi * (jari**2) * tinggi
-
-            result_text = f"Luas: {luas:.2f}, Volume: {volume:.2f}"
-            result_label.config(text=result_text)
-        except ValueError:
-            messagebox.showerror("Error", "Masukkan angka yang valid")
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = CalculatorApp(root)
-    root.mainloop()
+window.mainloop()
